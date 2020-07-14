13e9cd7a1c64d1f1f77e40990ce15f1093359e30
Wilkins Cheng <wilkinscheng@MacBook-Pro.hub>
2020-07-14 11:18:36 +1000

    setup basic backend and frontend

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import AppNavbar from "./components/AppNavbar";
+
+function App() {
+	return (
+		<div className="App">
+			<AppNavbar />
+		</div>
+	);
+}
+
+export default App;
---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+	mongoURI:
+		"mongodb+srv://Admin:wil123@merntodo.z490i.mongodb.net/Merntodo?retryWrites=true&w=majority",
+};
